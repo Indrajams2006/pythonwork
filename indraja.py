18a4fddb7634a18d89425f16d0f4c1f339b505d5
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-10-04 16:06:43 +0530

    Add files via upload

---
+++ indraja.py
@@ -0,0 +1,67 @@
+ #print("hello world")
+ #print("hello\nworld")
+'''
+if 5 > 2:
+ print("five is greater than two!")
+if 5 > 2:
+ print("five is greater than two!")
+ if 5 > 2:
+      pprint("five is greater than two!")
+ 
+if 5 > 2:
+  print("five is greater than two!")
+  print("five is greater than two!")
+ 
+
+  x = 5
+  x = "thomas"
+  print(x)
+
+x= str(3)
+y= int(3)
+z= float(3)
+print(x)
+print(y)
+print(z)
+x=5.1
+y="john"
+print(type(x))
+print(type(y))
+
+x,y,z="orange","banana","cherry"
+
+print(x)
+print(y)
+print(z)
+
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+x=" python"
+y=" is"
+z=" aweasome"
+print(x + y + z)
+
+username= input("Enter username:")
+input("username is: " + username)
+
+a=" hello"
+b=" world"
+c= a+b
+print(c)
+'''
+
+a= "hello, world!"
+print(a[6])
+
+b="hello, world!"
+print(b[:5])
+
+
+
+  
+
+
