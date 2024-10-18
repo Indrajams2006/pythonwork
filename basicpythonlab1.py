49e9a9b452a711b198937416f9f831f3074c132c
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-10-18 14:37:05 +0530

    Add files via upload
    Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.

---
+++ basicpythonlab1.py
@@ -0,0 +1,11 @@
+'''
+Author:indraja m s
+Date:18-10-2024
+Description:program that asks the user to input
+ their name and age. After receiving the input, display a message that includes the user's name
+ and age.
+ Hello, indraja! You are 17 years old.
+'''
+name=input("enter your name:")
+age=input("enter your age:")
+print("Hello,",name+"! you are ",age,"years old")
