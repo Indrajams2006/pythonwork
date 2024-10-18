4f9f8fdcf2e087dab4ce88eb8ba38c0b83e597f9
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-10-18 15:12:02 +0530

    Add files via upload
    a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.

---
+++ str.py
@@ -0,0 +1,13 @@
+'''
+Author:indraja m s
+Date:18-10-2024
+Description:a Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+str_variable="hello ,World"
+str_variable_another=str_variable[7:12]
+print(str_variable_another)
+str_3rd_variable=" Everyone!"
+print(str_variable_another+str_3rd_variable)
+
