73c675fc011717106fa10f311868fbf90ad8004a
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-10-19 15:42:21 +0530

    Add files via upload
     a Python program that performs basic arithmetic operators.

---
+++ arithmetic operators.py
@@ -0,0 +1,20 @@
+'''
+Author:indraja m s
+Date:19-10-24
+Description:A Python program that performs basic arithmetic operators.
+'''
+num1=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+num3=int(input("enter a number:"))
+sum=num1+num2
+print("the sum of num1 and num2 is:",sum)
+subtract=num2-num1
+print("the difference when num2 is subtracted from num1 is:",subtract)
+multiply=num1*num2
+print("the product of num1 and num2 is:",multiply)
+divide=num1/num2
+print("the quotient when num1 is divided by num2 is:",divide)
+modulus=num1%num2
+print("the remainder when num1 is diviuded by num2 is:",modulus)
+result = (num1 + num2) * num3 / 2
+print("the result of (num1 + num2) * num3 / 2 is: result",result)
